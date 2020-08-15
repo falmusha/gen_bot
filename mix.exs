05cbcf365c3236c9bbd96665e132e99b60b02596
Fahad Almusharraf <git@whatmail.org>
2020-08-15 05:00:16 -0400

    Add metadata for hex.pm

--- mix.exs
+++ mix.exs
@@ -11,19 +11,26 @@ defmodule GenBot.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:mox, "~> 0.3", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description() do
+    "Chatbot library with an API around the OTP statem module."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/falmusha/gen_bot"}
     ]
   end
 end
